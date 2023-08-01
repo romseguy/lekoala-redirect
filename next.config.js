92086800747352d379337beb5aad561f3c27b9ad
Romain Séguy <romseguy@users.noreply.github.com>
2023-08-01 12:23:59 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: 'https://lekoala.netlify.app',
+        destination: 'https://lebonforum.fr',
         permanent: true,
       },
     ];
