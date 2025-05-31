aa6bc34b8b3eb07b65b679bb1ab2c1f0322ab867
Romain Séguy <contact@romseguy.com>
2025-05-31 06:47:16 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: 'https://lebonforum.fr',
+        destination: 'https://romseguy.com',
         permanent: true,
       },
     ];
